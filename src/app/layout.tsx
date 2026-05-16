0b0aa476ab04d16d0381c89fb301d2cd3cc48c96
JzJoker <justinzhao1324@gmail.com>
2026-05-16 18:27:15 -0500

    config: allow local network dev origin and suppress extension hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${caveat.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans">{children}</body>
+      <body className="min-h-full flex flex-col font-sans" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
